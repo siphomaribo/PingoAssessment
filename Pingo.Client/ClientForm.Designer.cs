9d20571e40ee406f32831626915fb55610514e70
Sipho Maribo <mpho@mcmaribo.co.za>
2024-07-27 13:06:01 +0200

    Modified the form

--- Pingo.Client/ClientForm.Designer.cs
+++ Pingo.Client/ClientForm.Designer.cs
@@ -1,16 +1,11 @@
-﻿namespace Pingo.Client
+﻿using System.Xml.Linq;
+
+namespace Pingo.Client
 {
     partial class ClientForm
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,12 +15,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             dgvClients = new DataGridView();
@@ -39,49 +28,60 @@
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
+            dgvAddresses = new DataGridView();
+            btnAddAddress = new Button();
+            btnRemoveAddress = new Button();
+            lblSelectedAddress = new Label();
+            dgvContacts = new DataGridView();
+            btnAddContact = new Button();
+            btnRemoveContact = new Button();
+            lblSelectedContact = new Label();
+            label4 = new Label();
             ((System.ComponentModel.ISupportInitialize)dgvClients).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvAddresses).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dgvContacts).BeginInit();
             SuspendLayout();
             // 
             // dgvClients
             // 
             dgvClients.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvClients.Location = new Point(12, 288);
+            dgvClients.Location = new Point(11, 374);
             dgvClients.Name = "dgvClients";
-            dgvClients.Size = new Size(776, 150);
+            dgvClients.Size = new Size(1050, 215);
             dgvClients.TabIndex = 0;
             dgvClients.CellContentClick += dgvClients_CellContentClick;
             // 
             // txtId
             // 
-            txtId.Location = new Point(12, 15);
+            txtId.Location = new Point(11, 35);
             txtId.Name = "txtId";
             txtId.Size = new Size(100, 23);
             txtId.TabIndex = 1;
             // 
             // txtGender
             // 
-            txtGender.Location = new Point(568, 15);
+            txtGender.Location = new Point(507, 35);
             txtGender.Name = "txtGender";
             txtGender.Size = new Size(100, 23);
             txtGender.TabIndex = 2;
             // 
             // txtName
             // 
-            txtName.Location = new Point(171, 15);
+            txtName.Location = new Point(145, 35);
             txtName.Name = "txtName";
             txtName.Size = new Size(100, 23);
             txtName.TabIndex = 3;
             // 
             // dtpDateOfBirth
             // 
-            dtpDateOfBirth.Location = new Point(320, 12);
+            dtpDateOfBirth.Location = new Point(283, 35);
             dtpDateOfBirth.Name = "dtpDateOfBirth";
             dtpDateOfBirth.Size = new Size(200, 23);
             dtpDateOfBirth.TabIndex = 4;
             // 
             // btnAddClient
             // 
-            btnAddClient.Location = new Point(320, 243);
+            btnAddClient.Location = new Point(1076, 374);
             btnAddClient.Name = "btnAddClient";
             btnAddClient.Size = new Size(75, 23);
             btnAddClient.TabIndex = 5;
@@ -91,7 +91,7 @@
             // 
             // btnUpdateClient
             // 
-            btnUpdateClient.Location = new Point(423, 243);
+            btnUpdateClient.Location = new Point(1076, 403);
             btnUpdateClient.Name = "btnUpdateClient";
             btnUpdateClient.Size = new Size(75, 23);
             btnUpdateClient.TabIndex = 6;
@@ -101,7 +101,7 @@
             // 
             // btnDeleteClient
             // 
-            btnDeleteClient.Location = new Point(532, 243);
+            btnDeleteClient.Location = new Point(1076, 453);
             btnDeleteClient.Name = "btnDeleteClient";
             btnDeleteClient.Size = new Size(75, 23);
             btnDeleteClient.TabIndex = 7;
@@ -112,7 +112,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(569, 76);
+            label1.Location = new Point(507, 9);
             label1.Name = "label1";
             label1.Size = new Size(45, 15);
             label1.TabIndex = 8;
@@ -121,7 +121,7 @@
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(171, 76);
+            label2.Location = new Point(145, 12);
             label2.Name = "label2";
             label2.Size = new Size(39, 15);
             label2.TabIndex = 9;
@@ -130,17 +130,111 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(12, 76);
+            label3.Location = new Point(12, 12);
             label3.Name = "label3";
             label3.Size = new Size(54, 15);
             label3.TabIndex = 10;
             label3.Text = "Surname";
             // 
+            // dgvAddresses
+            // 
+            dgvAddresses.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvAddresses.Location = new Point(12, 129);
+            dgvAddresses.Name = "dgvAddresses";
+            dgvAddresses.Size = new Size(283, 100);
+            dgvAddresses.TabIndex = 13;
+            dgvAddresses.CellContentClick += dgvAddresses_CellContentClick;
+            // 
+            // btnAddAddress
+            // 
+            btnAddAddress.Location = new Point(12, 235);
+            btnAddAddress.Name = "btnAddAddress";
+            btnAddAddress.Size = new Size(75, 23);
+            btnAddAddress.TabIndex = 14;
+            btnAddAddress.Text = "Add Address";
+            btnAddAddress.UseVisualStyleBackColor = true;
+            btnAddAddress.Click += btnAddAddress_Click;
+            // 
+            // btnRemoveAddress
+            // 
+            btnRemoveAddress.Location = new Point(114, 235);
+            btnRemoveAddress.Name = "btnRemoveAddress";
+            btnRemoveAddress.Size = new Size(100, 23);
+            btnRemoveAddress.TabIndex = 15;
+            btnRemoveAddress.Text = "Remove Address";
+            btnRemoveAddress.UseVisualStyleBackColor = true;
+            btnRemoveAddress.Click += btnRemoveAddress_Click;
+            // 
+            // lblSelectedAddress
+            // 
+            lblSelectedAddress.AutoSize = true;
+            lblSelectedAddress.Location = new Point(12, 270);
+            lblSelectedAddress.Name = "lblSelectedAddress";
+            lblSelectedAddress.Size = new Size(99, 15);
+            lblSelectedAddress.TabIndex = 16;
+            lblSelectedAddress.Text = "Selected Address:";
+            // 
+            // dgvContacts
+            // 
+            dgvContacts.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvContacts.Location = new Point(605, 129);
+            dgvContacts.Name = "dgvContacts";
+            dgvContacts.Size = new Size(279, 100);
+            dgvContacts.TabIndex = 19;
+            dgvContacts.CellContentClick += dgvContacts_CellContentClick;
+            // 
+            // btnAddContact
+            // 
+            btnAddContact.Location = new Point(605, 235);
+            btnAddContact.Name = "btnAddContact";
+            btnAddContact.Size = new Size(75, 23);
+            btnAddContact.TabIndex = 20;
+            btnAddContact.Text = "Add Contact";
+            btnAddContact.UseVisualStyleBackColor = true;
+            btnAddContact.Click += btnAddContact_Click;
+            // 
+            // btnRemoveContact
+            // 
+            btnRemoveContact.Location = new Point(711, 235);
+            btnRemoveContact.Name = "btnRemoveContact";
+            btnRemoveContact.Size = new Size(100, 23);
+            btnRemoveContact.TabIndex = 21;
+            btnRemoveContact.Text = "Remove Contact";
+            btnRemoveContact.UseVisualStyleBackColor = true;
+            btnRemoveContact.Click += btnRemoveContact_Click;
+            // 
+            // lblSelectedContact
+            // 
+            lblSelectedContact.AutoSize = true;
+            lblSelectedContact.Location = new Point(605, 279);
+            lblSelectedContact.Name = "lblSelectedContact";
+            lblSelectedContact.Size = new Size(99, 15);
+            lblSelectedContact.TabIndex = 22;
+            lblSelectedContact.Text = "Selected Contact:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(283, 9);
+            label4.Name = "label4";
+            label4.Size = new Size(73, 15);
+            label4.TabIndex = 23;
+            label4.Text = "Date of Birth";
+            // 
             // ClientForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1223, 615);
+            Controls.Add(label4);
+            Controls.Add(lblSelectedContact);
+            Controls.Add(btnRemoveContact);
+            Controls.Add(btnAddContact);
+            Controls.Add(dgvContacts);
+            Controls.Add(lblSelectedAddress);
+            Controls.Add(btnRemoveAddress);
+            Controls.Add(btnAddAddress);
+            Controls.Add(dgvAddresses);
             Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
@@ -155,11 +249,11 @@
             Name = "ClientForm";
             Text = "ClientForm";
             ((System.ComponentModel.ISupportInitialize)dgvClients).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvAddresses).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dgvContacts).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
-
-        #endregion
 
         private DataGridView dgvClients;
         private TextBox txtId;
@@ -172,5 +266,14 @@
         private Label label1;
         private Label label2;
         private Label label3;
+        private DataGridView dgvAddresses;
+        private Button btnAddAddress;
+        private Button btnRemoveAddress;
+        private Label lblSelectedAddress;
+        private DataGridView dgvContacts;
+        private Button btnAddContact;
+        private Button btnRemoveContact;
+        private Label lblSelectedContact;
+        private Label label4;
     }
 }
--- Pingo.Client/ClientForm.cs
+++ Pingo.Client/ClientForm.cs
@@ -1,45 +1,51 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Pingo.Models;
 
 namespace Pingo.Client
 {
     public partial class ClientForm : Form
     {
+        private Pingo.Models.Client _currentClient;
+
         public ClientForm()
         {
             InitializeComponent();
             LoadClients();
-
+            LoadAddress();
+            LoadContacts();
         }
 
         private async void LoadClients()
         {
-            var clients = await ApiClient.GetAsync<IEnumerable<Models.Client>>("client");
+            var clients = await ApiClient.GetAsync<IEnumerable<Pingo.Models.Client>>("client");
             dgvClients.DataSource = clients;
         }
 
-
-
-        private void dgvClients_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private async void LoadAddress()
         {
+            var addresses = await ApiClient.GetAsync<IEnumerable<Models.Address>>("address");
+            dgvAddresses.DataSource = addresses;
+        }
 
+        private async void LoadContacts()
+        {
+            var contacts = await ApiClient.GetAsync<IEnumerable<Contact>>("contact");
+            dgvContacts.DataSource = contacts;
         }
 
         private async void btnAddClient_Click(object sender, EventArgs e)
         {
-            var client = new Models.Client
+            var client = new Pingo.Models.Client
             {
                 Id = Guid.NewGuid(),
                 Name = txtName.Text,
                 Gender = txtGender.Text,
-                DateOfBirth = dtpDateOfBirth.Value
+                DateOfBirth = dtpDateOfBirth.Value,
+                AddressIds = new List<Guid>(),
+                ContactIds = new List<Guid>()
             };
 
             await ApiClient.PostAsync("client", client);
@@ -48,23 +54,120 @@ namespace Pingo.Client
 
         private async void btnUpdateClient_Click(object sender, EventArgs e)
         {
-            var client = new Models.Client
-            {
-                Id = Guid.Parse(txtId.Text),
-                Name = txtName.Text,
-                Gender = txtGender.Text,
-                DateOfBirth = dtpDateOfBirth.Value
-            };
+            if (_currentClient == null)
+                return;
 
-            await ApiClient.PutAsync($"client/{client.Id}", client);
+            _currentClient.Name = txtName.Text;
+            _currentClient.Gender = txtGender.Text;
+            _currentClient.DateOfBirth = dtpDateOfBirth.Value;
+
+            await ApiClient.PutAsync($"client/{_currentClient.Id}", _currentClient);
             LoadClients();
         }
 
         private async void btnDeleteClient_Click(object sender, EventArgs e)
         {
-            var clientId = Guid.Parse(txtId.Text);
-            await ApiClient.DeleteAsync($"client/{clientId}");
+            if (_currentClient == null)
+                return;
+
+            await ApiClient.DeleteAsync($"client/{_currentClient.Id}");
             LoadClients();
+        }
+
+        private void dgvAddresses_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == 0) // Assuming the first column is the select button
+            {
+                var addressId = (Guid)dgvAddresses.Rows[e.RowIndex].Cells["Id"].Value; // Replace "Id" with the actual column name
+                if (!_currentClient.AddressIds.Contains(addressId))
+                {
+                    _currentClient.AddressIds.Add(addressId);
+                    lblSelectedAddress.Text = $"Selected Address ID: {addressId}";
+                }
+            }
+        }
+
+        private void dgvContacts_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex == 0) // Assuming the first column is the select button
+            {
+                var contactId = (Guid)dgvContacts.Rows[e.RowIndex].Cells["Id"].Value; // Replace "Id" with the actual column name
+                if (!_currentClient.ContactIds.Contains(contactId))
+                {
+                    _currentClient.ContactIds.Add(contactId);
+                    lblSelectedContact.Text = $"Selected Contact ID: {contactId}";
+                }
+            }
+        }
+
+        private void btnAddAddress_Click(object sender, EventArgs e)
+        {
+            // Logic to add selected address to the client
+            // This could be handled by selecting from DataGridView and adding to the collection
+        }
+
+        private void btnRemoveAddress_Click(object sender, EventArgs e)
+        {
+            // Logic to remove selected address from the client
+            // This could be handled by removing from AddressIds
+            var addressId = GetSelectedAddressId();
+            if (addressId != Guid.Empty)
+            {
+                _currentClient.AddressIds.Remove(addressId);
+                lblSelectedAddress.Text = "Address removed.";
+            }
+        }
+
+        private void btnAddContact_Click(object sender, EventArgs e)
+        {
+            // Logic to add selected contact to the client
+            // This could be handled by selecting from DataGridView and adding to the collection
+        }
+
+        private void btnRemoveContact_Click(object sender, EventArgs e)
+        {
+            // Logic to remove selected contact from the client
+            // This could be handled by removing from ContactIds
+            var contactId = GetSelectedContactId();
+            if (contactId != Guid.Empty)
+            {
+                _currentClient.ContactIds.Remove(contactId);
+                lblSelectedContact.Text = "Contact removed.";
+            }
+        }
+
+        private Guid GetSelectedAddressId()
+        {
+            // Implement logic to get selected address ID
+            return Guid.Empty;
+        }
+
+        private Guid GetSelectedContactId()
+        {
+            // Implement logic to get selected contact ID
+            return Guid.Empty;
+        }
+        private async void dgvClients_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                var clientId = (Guid)dgvClients.Rows[e.RowIndex].Cells["Id"].Value; // Adjust "Id" to match your column name
+                _currentClient = await ApiClient.GetAsync<Models.Client>($"client/{clientId}");
+                PopulateClientDetails();
+            }
+        }
+
+        private void PopulateClientDetails()
+        {
+            if (_currentClient != null)
+            {
+                txtId.Text = _currentClient.Id.ToString();
+                txtName.Text = _currentClient.Name;
+                txtGender.Text = _currentClient.Gender;
+                dtpDateOfBirth.Value = _currentClient.DateOfBirth ?? DateTime.Now;
+
+                // Update Address and Contact displays based on _currentClient.AddressIds and _currentClient.ContactIds
+            }
         }
     }
 }
